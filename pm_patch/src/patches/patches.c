8fcfe1ecd4ac33430c71ebb4ac7657459e0ee87c
konmai69 <deeznutshagottem69@outlook.com>
2023-08-19 03:33:35 +0700

    Update patches.c
    Changed addresses to match with the European exe (M-108EU)

--- pm_patch/src/patches/patches.c
+++ pm_patch/src/patches/patches.c
@@ -9,15 +9,15 @@
 
 // Hardcoded Addresses for Patches
 #define ADDR_SDL_VIDEOFLAGS     0x08056BC8
-#define ADDR_MENU_QCTEST        0x0807508C
-#define ADDR_MENU_DEVTEST       0x08076884
+#define ADDR_MENU_QCTEST        0x08075088
+#define ADDR_MENU_DEVTEST       0x08076880
 #define ADDR_MENU_TRACKBALL     0x08069768
 #define ADDR_CALL_OPMENU        0x08056A78
-#define ADDR_SKIP_WARNING       0x080A7D04
-#define ADDR_SKIP_WARNING2      0x080A7DBD
-#define ADDR_ENABLE_AUTOPLAY    0x080A7DC7
-#define ADDR_MENU_LANGUAGE      0x080739EC
-#define ADDR_OSS_SOUNDFIX       0x0807C34F
+#define ADDR_SKIP_WARNING       0x080A7C84
+#define ADDR_SKIP_WARNING2      0x080A7D3D
+#define ADDR_ENABLE_AUTOPLAY    0x080A7D47
+#define ADDR_MENU_LANGUAGE      0x080739E8
+#define ADDR_OSS_SOUNDFIX       0x0807C34B
 #define ADDR_REAL_SOUNDPLAY     0x08065524
 #define ADDR_SOUND_CH_TABLE     0x083EF020
 
@@ -73,80 +73,80 @@ static char* difficulty_options[] = {
 };
 
 static StrReplaceEntry test_menu_english[] = {
-    {0x0807B1BD,"[%d:%2d] Notes:%d Tempo:%d Moves:%d"},
-    {0x0807B1EA,"Stars: %02d SecID: %d Difficulty: %s"},
-    {0x0807B202,"Select Song"},
-    {0x0807B22B,(char*)difficulty_options},
-    {0x0807B230,"Mode: %s"},
-    {0x0807B256,(char*)noyes},
-    {0x0807B265,"%dP Enabled: %s"},
-    {0x0807B2AD,"%dP Autoplay: %s"},
-    {0x0807B2D3,(char*)speed_options},
-    {0x0807B2DF,"%dP Speed Mod: %s"},
-    {0x0807B308,(char*)cloak_options},
-    {0x0807B30E,"%dP Cloak Mod: %s"},
-    {0x0807B334,"%dP Noteskin: %d"},
-    {0x0807B359,(char*)record_options},
-    {0x0807B35E,"KeyRecord: %s"},
-    {0x0807B386,"File Index: %d"},
-    {0x0807B3AF,(char*)yesno},
-    {0x0807B3B4,"Keysounds: %s"},
-    {0x0807B3E0,"Player Vol: %d"},
-    {0x0807B40C,"Song Vol: %d"},
-    {0x0807B438,"BG Vol: %d"},
-    {0x0807B461,(char*)judgement_options},
-    {0x0807B466,"Judgement: %s"},
+    {0x0807B1B9,"[%d:%2d] Notes:%d Tempo:%d Moves:%d"},
+    {0x0807B1E6,"Stars: %02d SecID: %d Difficulty: %s"},
+    {0x0807B1FD,"Select Song"},
+    {0x0807B227,(char*)difficulty_options},
+    {0x0807B22B,"Mode: %s"},
+    {0x0807B252,(char*)noyes},
+    {0x0807B261,"%dP Enabled: %s"},
+    {0x0807B2A9,"%dP Autoplay: %s"},
+    {0x0807B2CF,(char*)speed_options},
+    {0x0807B2DB,"%dP Speed Mod: %s"},
+    {0x0807B304,(char*)cloak_options},
+    {0x0807B309,"%dP Cloak Mod: %s"},
+    {0x0807B32F,"%dP Noteskin: %d"},
+    {0x0807B352,(char*)record_options},
+    {0x0807B359,"KeyRecord: %s"},
+    {0x0807B381,"File Index: %d"},
+    {0x0807B3A8,(char*)yesno},
+    {0x0807B3AF,"Keysounds: %s"},
+    {0x0807B3DB,"Player Vol: %d"},
+    {0x0807B407,"Song Vol: %d"},
+    {0x0807B433,"BG Vol: %d"},
+    {0x0807B45A,(char*)judgement_options},
+    {0x0807B462,"Judgement: %s"},
 };
 
 // Linux Path Replacements
 static StrReplaceEntry linux_path_entries[] = {
-    {0x08172040,"./band1/rom/chainl.rom"},
-    {0x08172140,"./band1/rom/effect3l.rom"},
-    {0x08172240,"./band1/rom/churchl.rom"},
-    {0x08172340,"./band1/rom/effect3l.rom"},
-    {0x08172440,"./band1/rom/starnightl.rom"},
-    {0x08172540,"./band1/rom/effect2l.rom"},
-    {0x08172640,"./band1/rom/herol.rom"},
-    {0x08172740,"./band1/rom/effect1l.rom"},
-    {0x08172840,"./band1/rom/punkl.rom"},
-    {0x08172940,"./band1/rom/effect1l.rom"},
-    {0x08172A40,"./band1/rom/taiwangol.rom"},
-    {0x08172B40,"./band1/rom/effect3l.rom"},
-    {0x08172C40,"./band1/rom/lovewordl.rom"},
-    {0x08172D40,"./band1/rom/effect2l.rom"},
-    {0x08172E40,"./band1/rom/ancientl.rom"},
-    {0x08172F40,"./band1/rom/effect1l.rom"},
-    {0x08173040,"./band1/rom/ninjal.rom"},
-    {0x08173140,"./band1/rom/effect2l.rom"},
-    {0x08173240,"./band1/rom/spacel.rom"},
-    {0x08173340,"./band1/rom/effect1l.rom"},
-    {0x08173440,"./band1/rom/baseballl.rom"},
-    {0x08173540,"./band1/rom/effect3l.rom"},
-    {0x08173640,"./band1/rom/summerl.rom"},
-    {0x08173740,"./band1/rom/effect3l.rom"},
-    {0x08173840,"./band1/rom/bigcityl.rom"},
-    {0x08173940,"./band1/rom/effect3l.rom"},
-    {0x08173A40,"./band1/rom/wildwestl.rom"},
-    {0x08173B40,"./band1/rom/effect2l.rom"},
-    {0x081200B7,"./main/selmodel.rom"},
-    {0x081200D2,"./main/rolel.rom"},
-    {0x081200EA,"./main/role1l.rom"},
-    {0x08120103,"./main/role2l.rom"},
-    {0x0812011C,"./main/role3l.rom"},
-    {0x08120135,"./main/role4l.rom"},
-    {0x081201EF,"./main/coinpagel.rom"},
-    {0x08120325,"./main/photol.rom"},
-    {0x0812073C,"./main/selsongl.rom"},
-    {0x081207C5,"./main/optionl.rom"},
-    {0x08122964,"./main/selrolel.rom"},
-    {0x081229DF,"./main/cardinfol.rom"},
-    {0x08122F97,"./main/signl.rom"},
-    {0x08123045,"./main/resultl.rom"},
-    {0x08123537,"./main/irl.rom"},
-    {0x08123575,"./main/rankl.rom"},
-    {0x0812364D,"./main/stylel.rom"},
-    {0x0816E7E0,"write-avi.so"},
-    {0x0816E820,"snd-oss.so"}
+    {0x08171F40,"./band1/rom/chainl.rom"},
+    {0x08172040,"./band1/rom/effect3l.rom"},
+    {0x08172140,"./band1/rom/churchl.rom"},
+    {0x08172240,"./band1/rom/effect3l.rom"},
+    {0x08172340,"./band1/rom/starnightl.rom"},
+    {0x08172440,"./band1/rom/effect2l.rom"},
+    {0x08172540,"./band1/rom/herol.rom"},
+    {0x08172640,"./band1/rom/effect1l.rom"},
+    {0x08172740,"./band1/rom/punkl.rom"},
+    {0x08172840,"./band1/rom/effect1l.rom"},
+    {0x08172940,"./band1/rom/taiwangol.rom"},
+    {0x08172A40,"./band1/rom/effect3l.rom"},
+    {0x08172B40,"./band1/rom/lovewordl.rom"},
+    {0x08172C40,"./band1/rom/effect2l.rom"},
+    {0x08172D40,"./band1/rom/ancientl.rom"},
+    {0x08172E40,"./band1/rom/effect1l.rom"},
+    {0x08172F40,"./band1/rom/ninjal.rom"},
+    {0x08173040,"./band1/rom/effect2l.rom"},
+    {0x08173140,"./band1/rom/spacel.rom"},
+    {0x08173240,"./band1/rom/effect1l.rom"},
+    {0x08173340,"./band1/rom/baseballl.rom"},
+    {0x08173440,"./band1/rom/effect3l.rom"},
+    {0x08173540,"./band1/rom/summerl.rom"},
+    {0x08173640,"./band1/rom/effect3l.rom"},
+    {0x08173740,"./band1/rom/bigcityl.rom"},
+    {0x08173840,"./band1/rom/effect3l.rom"},
+    {0x08173940,"./band1/rom/wildwestl.rom"},
+    {0x08173A40,"./band1/rom/effect2l.rom"},
+    {0x08120037,"./main/selmodel.rom"},
+    {0x08120052,"./main/rolel.rom"},
+    {0x0812006A,"./main/role1l.rom"},
+    {0x08120083,"./main/role2l.rom"},
+    {0x0812009C,"./main/role3l.rom"},
+    {0x081200B5,"./main/role4l.rom"},
+    {0x0812016F,"./main/coinpagel.rom"},
+    {0x081202A5,"./main/photol.rom"},
+    {0x081206BC,"./main/selsongl.rom"},
+    {0x08120745,"./main/optionl.rom"},
+    {0x081228E4,"./main/selrolel.rom"},
+    {0x0812295F,"./main/cardinfol.rom"},
+    {0x08122F17,"./main/signl.rom"},
+    {0x08122FC5,"./main/resultl.rom"},
+    {0x081234B7,"./main/irl.rom"},
+    {0x081234F5,"./main/rankl.rom"},
+    {0x081235CD,"./main/stylel.rom"},
+    {0x0816E6E0,"write-avi.so"},
+    {0x0816E720,"snd-oss.so"}
 };
 
 // Localize the Song Test Menu 
